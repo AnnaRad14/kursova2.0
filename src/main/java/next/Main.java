678253e087985406bb187b1dd7cd5be444584b1d
AnnaRad14 <145984789+AnnaRad14@users.noreply.github.com>
2026-01-28 18:38:57 +0200

    Initial commit

---
+++ src/main/java/next/Main.java
@@ -0,0 +1,21 @@
+package next;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String correctPassword = "555";
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введіть пароль: ");
+        String userPassword = scanner.nextLine();
+
+        if (userPassword.equals(correctPassword)) {
+            System.out.println("Доступ дозволено");
+        } else {
+            System.out.println("Невірний пароль");
+        }
+
+        scanner.close();
+    }
+}
